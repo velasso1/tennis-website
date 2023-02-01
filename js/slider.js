cd750c597cfd286095baed04a58ba06e0aceecbc
Alexandr Vasin <alexandr.vasin1@yandex.ru>
2023-02-02 00:01:31 +0300

    add slider

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,25 @@
 'use strict;'
+
+import Swiper from 'swiper';
+
+const swiper = new Swiper('.swiper2', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
